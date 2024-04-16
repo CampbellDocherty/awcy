1eb00cbfe019b5bb627aabb10efd5dde87585db6
CampbellDocherty <55450462+CampbellDocherty@users.noreply.github.com>
2024-04-16 18:25:13 +0000

    Deploying to gh-pages from @ CampbellDocherty/awcy@9d66310d9c38f2bd9680879d8886305de606f4d2 🚀

---
+++ assets/LazyImage-fbe09e7d.js
@@ -0,0 +1,10 @@
+import{u as o,j as t}from"./index-26136d36.js";const i=o.article`
+  margin-bottom: 30px;
+`,c=o.section`
+  overflow: hidden;
+  display: block;
+`,a=o.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`,s=({src:e})=>t(i,{children:t(c,{children:t(a,{src:e,alt:"blog post"})})});export{s as default};
