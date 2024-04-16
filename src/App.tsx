b1ab502d5989425146c41f23c31e8b9beb48131a
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-04-16 19:04:01 +0100

    Add tee image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { Header, Subtitle, Title } from './styles/header.styles';
+import { Header, HeaderImage, Subtitle, Title } from './styles/header.styles';
 import { Container, Main } from './styles/main.styles';
+import tee from './assets/tee.png';
 
 const App = () => {
   return (
     <>
       <Header>
+        <HeaderImage>
+          <img src={tee} alt="are we cool yet t-shirt" />
+        </HeaderImage>
         <Title>arewecoolyet.blog</Title>
         <Subtitle>YOU&apos;RE ALL WELCOME</Subtitle>
       </Header>
