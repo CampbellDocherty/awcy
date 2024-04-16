4d3eb3f8f641cc326c3148404b27eb3b0845d034
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-04-16 18:37:35 +0100

    Style and new style files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Header, Subtitle, Title } from './styles/header.styles';
+import { Container, Main } from './styles/main.styles';
+
 const App = () => {
   return (
     <>
-      <h1>arewecoolyet.blog</h1>
-      <p>YOU&apos;RE ALL WELCOME</p>
+      <Header>
+        <Title>arewecoolyet.blog</Title>
+        <Subtitle>YOU&apos;RE ALL WELCOME</Subtitle>
+      </Header>
+      <Container>
+        <Main />
+      </Container>
     </>
   );
 };
