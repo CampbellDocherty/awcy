3ef2daf9a38eb342e74b8ddf83294db803e9b3df
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-04-16 19:19:33 +0100

    Add blog images

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Header, HeaderImage, Subtitle, Title } from './styles/header.styles';
 import { Container, Main } from './styles/main.styles';
 import tee from './assets/tee.png';
+import { blogImages } from './assets/blogImages';
+import { Article, Section, BlogImage } from './styles/image.styles';
 
 const App = () => {
   return (
@@ -13,7 +15,15 @@ const App = () => {
         <Subtitle>YOU&apos;RE ALL WELCOME</Subtitle>
       </Header>
       <Container>
-        <Main />
+        <Main>
+          {blogImages.map((image, index) => (
+            <Article key={index}>
+              <Section>
+                <BlogImage src={image} alt="blog post" />
+              </Section>
+            </Article>
+          ))}
+        </Main>
       </Container>
     </>
   );
