867ddcc407578b06e0bde72c9092b337844db829
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-04-20 11:43:04 +0100

    Add upload and get

--- src/firebase/storage.ts
+++ src/firebase/storage.ts
@@ -1,9 +1,16 @@
-import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  listAll,
+  getDownloadURL,
+  getMetadata,
+  // eslint-disable-next-line import/named
+  FullMetadata,
+} from 'firebase/storage';
 import { app } from './app';
 
 const storage = getStorage(app);
-
-// Upload file and metadata to the object 'images/mountains.jpg'
 
 type Metadata = {
   name: string;
@@ -14,6 +21,27 @@ export const uploadFile = (file: File, metadata: Metadata) => {
   const storageRef = ref(storage, metadata.name);
   uploadBytesResumable(storageRef, file, {
     contentType: file.type,
+    cacheControl: 'public,max-age=3600',
     customMetadata: metadata,
   });
+};
+
+export type FirebaseStorageContent = {
+  downloadUrl: string;
+  metadata: FullMetadata;
+};
+
+export const getFiles = async () => {
+  const res = await listAll(ref(storage));
+  const promisedFiles = res.items.map(async (item) => {
+    const storageRef = ref(storage, item.fullPath);
+    const downloadUrl = await getDownloadURL(storageRef);
+    const metadata = await getMetadata(storageRef);
+    return {
+      downloadUrl,
+      metadata,
+    };
+  });
+  const files: FirebaseStorageContent[] = await Promise.all(promisedFiles);
+  return files;
 };
