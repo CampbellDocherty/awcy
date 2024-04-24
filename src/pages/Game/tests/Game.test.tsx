cf1e999179bc7f53f154806523339f4b8e59ee72
Campbell Docherty <campbell.docherty@oaknorth.co.uk>
2024-04-24 19:18:25 +0100

    Fix test

--- src/pages/Game/tests/Game.test.tsx
+++ src/pages/Game/tests/Game.test.tsx
@@ -1,22 +1,22 @@
 import { describe, expect, test } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { GameWrapper } from '../GameWrapper';
+import { Game } from '../Game';
 
 describe('When the game renders', () => {
   test('it shows the title', () => {
-    render(<GameWrapper />);
+    render(<Game />);
     screen.getByText('AWCY');
     screen.getByText('version 1.0');
   });
 
   test('it shows the first login screen', () => {
-    render(<GameWrapper />);
+    render(<Game />);
     screen.getByText('Select character name:');
     screen.getByText('Play');
   });
 
   test('goes to club when submit name', () => {
-    render(<GameWrapper />);
+    render(<Game />);
     const input = screen.getByLabelText('Select character name:');
     fireEvent.change(input, { target: { value: 'Cammy' } });
     fireEvent.click(screen.getByText('Play'));
@@ -26,7 +26,7 @@ describe('When the game renders', () => {
   });
 
   test('goes home when right arrow clicked', () => {
-    render(<GameWrapper />);
+    render(<Game />);
     const input = screen.getByLabelText('Select character name:');
     fireEvent.change(input, { target: { value: 'Cammy' } });
     fireEvent.click(screen.getByText('Play'));
