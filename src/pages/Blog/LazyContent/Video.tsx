5b544b4f60e990a51613c5a389f37b677339927d
Campbell Docherty <campbell.docherty@oaknorth.co.uk>
2024-04-25 20:52:53 +0100

    Expand on click

--- src/pages/Blog/LazyContent/Video.tsx
+++ src/pages/Blog/LazyContent/Video.tsx
@@ -2,9 +2,13 @@ import { FirebaseStorageContent } from '../../../firebase/storage';
 import { BlogVideo } from '../styles/image.styles';
 import { ContentType } from '.';
 
-export const Video = ({ content }: { content: FirebaseStorageContent }) => (
-  <BlogVideo loop autoPlay playsInline muted>
-    <source src={content.downloadUrl} type={ContentType.MP4} />
-    <meta itemProp="name" content={content.metadata.name}></meta>
+interface VideoProps extends React.HTMLAttributes<HTMLVideoElement> {
+  firebaseContent: FirebaseStorageContent;
+}
+
+export const Video = ({ firebaseContent, onClick }: VideoProps) => (
+  <BlogVideo loop autoPlay playsInline muted onClick={onClick}>
+    <source src={firebaseContent.downloadUrl} type={ContentType.MP4} />
+    <meta itemProp="name" content={firebaseContent.metadata.name}></meta>
   </BlogVideo>
 );
--- src/pages/Blog/LazyContent/index.tsx
+++ src/pages/Blog/LazyContent/index.tsx
@@ -1,7 +1,14 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../../context/User';
 import { FirebaseStorageContent } from '../../../firebase/storage';
-import { Article, BlogImage, Caption, Section } from '../styles/image.styles';
+import {
+  Article,
+  BlogImage,
+  Caption,
+  Modal,
+  ModalContent,
+  Section,
+} from '../styles/image.styles';
 import { ContentManagement } from './ContentManagement';
 import { Video } from './Video';
 
@@ -22,31 +29,68 @@ const LazyImage = ({
     content.metadata.customMetadata?.caption || ''
   );
 
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const isVideo =
     content.metadata.contentType === ContentType.MP4 ||
     content.metadata.contentType === ContentType.QUICKTIME;
 
+  const onExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  useEffect(() => {
+    if (isExpanded) {
+      document.body.style.overflow = 'hidden';
+    }
+
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [isExpanded]);
+
   return (
-    <Article>
-      <div>
-        <Section>
-          {isVideo ? (
-            <Video content={content} />
-          ) : (
-            <BlogImage src={content.downloadUrl} alt={content.metadata.name} />
-          )}
-        </Section>
-        {caption && <Caption>{caption}</Caption>}
-      </div>
-      {user && (
-        <ContentManagement
-          content={content}
-          onDelete={onDelete}
-          caption={caption}
-          setCaption={setCaption}
-        />
+    <>
+      {isExpanded && (
+        <Modal onClick={onExpand}>
+          <ModalContent>
+            {isVideo ? (
+              <Video firebaseContent={content} />
+            ) : (
+              <BlogImage
+                src={content.downloadUrl}
+                alt={content.metadata.name}
+              />
+            )}
+          </ModalContent>
+        </Modal>
       )}
-    </Article>
+
+      <Article>
+        <div>
+          <Section>
+            {isVideo ? (
+              <Video firebaseContent={content} onClick={onExpand} />
+            ) : (
+              <BlogImage
+                onClick={onExpand}
+                src={content.downloadUrl}
+                alt={content.metadata.name}
+              />
+            )}
+          </Section>
+          {caption && <Caption>{caption}</Caption>}
+        </div>
+        {user && (
+          <ContentManagement
+            content={content}
+            onDelete={onDelete}
+            caption={caption}
+            setCaption={setCaption}
+          />
+        )}
+      </Article>
+    </>
   );
 };
 
