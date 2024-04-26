87a334699f15cc7f895a43a19e73f3b81fd4c607
Campbell Docherty <campbell.docherty@oaknorth.co.uk>
2024-04-26 20:04:41 +0100

    New splash screen

--- src/pages/Blog/Splash/index.tsx
+++ src/pages/Blog/Splash/index.tsx
@@ -1,8 +1,34 @@
-import { useRef } from 'react';
-import splash from '../../../assets/splash.mp4';
+import { useEffect, useState } from 'react';
+import aComputer from '../../../assets/A.png';
+import cComputer from '../../../assets/C.png';
+import wComputer from '../../../assets/W.png';
+import yComputer from '../../../assets/Y.png';
+
+const images = [aComputer, wComputer, cComputer, yComputer];
 
 export const Splash = ({ onEnded }: { onEnded: () => void }) => {
-  const splashRef = useRef<HTMLVideoElement>(null);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const delayBeforeNavigation = 5000;
+  const delayBetweenImages = 400;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, delayBetweenImages);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const navigationTimeout = setInterval(() => {
+      onEnded();
+    }, delayBeforeNavigation);
+
+    return () => clearTimeout(navigationTimeout);
+  }, []);
+
   return (
     <div
       style={{
@@ -14,14 +40,10 @@ export const Splash = ({ onEnded }: { onEnded: () => void }) => {
         backgroundColor: 'white',
       }}
     >
-      <video
-        onEnded={onEnded}
-        ref={splashRef}
-        autoPlay
-        muted
-        playsInline
-        src={splash}
-        style={{ width: '100%', height: '100%', maxWidth: '900px' }}
+      <img
+        style={{ width: '80%', maxWidth: '300px' }}
+        src={images[currentImageIndex]}
+        alt={`hey`}
       />
     </div>
   );
