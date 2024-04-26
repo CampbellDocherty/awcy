8d972f5685684a04b350c91068f2c32eeeb0fcf8
Campbell Docherty <campbell.docherty@oaknorth.co.uk>
2024-04-26 21:10:39 +0100

    New splash

--- src/pages/Blog/Splash/index.tsx
+++ src/pages/Blog/Splash/index.tsx
@@ -1,50 +1,64 @@
 import { useEffect, useState } from 'react';
+import { Grid, SplashContainer, SplashImage } from './styles';
+
 import aComputer from '../../../assets/A.png';
 import cComputer from '../../../assets/C.png';
 import wComputer from '../../../assets/W.png';
 import yComputer from '../../../assets/Y.png';
 
-const images = [aComputer, wComputer, cComputer, yComputer];
-
 export const Splash = ({ onEnded }: { onEnded: () => void }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const delayBeforeNavigation = 5000;
-  const delayBetweenImages = 400;
+  const animationDuration = 4000;
+  const navigateAwayAfter = 4500;
+  const delayBetweenImages = 500;
+
+  const [images, setImages] = useState([
+    aComputer,
+    wComputer,
+    cComputer,
+    yComputer,
+  ]);
+  const [showOriginalOrder, setShowOriginalOrder] = useState(false);
+  const [imageIndex, setImageIndex] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
-      );
-    }, delayBetweenImages);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    const navigationTimeout = setInterval(() => {
+    const navigationTimeout = setTimeout(() => {
       onEnded();
-    }, delayBeforeNavigation);
+    }, navigateAwayAfter);
 
     return () => clearTimeout(navigationTimeout);
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setImageIndex((prevIndex) => (prevIndex + 1) % 4);
+    }, delayBetweenImages);
+
+    setTimeout(() => {
+      setImages([aComputer, wComputer, cComputer, yComputer]);
+      setShowOriginalOrder(true);
+      clearInterval(interval);
+    }, animationDuration);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div
-      style={{
-        width: '100vw',
-        height: window.innerHeight,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
-      }}
-    >
-      <img
-        style={{ width: '80%', maxWidth: '300px' }}
-        src={images[currentImageIndex]}
-        alt={`hey`}
-      />
-    </div>
+    <SplashContainer>
+      <Grid>
+        {[0, 1, 2, 3].map((index) => {
+          const imageIndexToRender = showOriginalOrder
+            ? index
+            : (imageIndex + index) % 4;
+          return (
+            <SplashImage
+              key={index}
+              $show={true}
+              src={images[imageIndexToRender]}
+              alt={`loading computer ${index}`}
+            />
+          );
+        })}
+      </Grid>
+    </SplashContainer>
   );
 };
