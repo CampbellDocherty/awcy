6da17c1b7d26a6e15a0eed52f8d8366dfac58f1c
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-05-03 20:42:52 +0100

    Flyer at top

--- src/pages/Blog/columnCalculator/splitArray.tsx
+++ src/pages/Blog/columnCalculator/splitArray.tsx
@@ -4,6 +4,10 @@ export const splitArray = (
   content: FirebaseStorageContent[],
   columns: number
 ) => {
+  const flyer = content.find((item) => item.metadata.name === 'flyer');
+  const contentWithoutFlyer = content.filter(
+    (item) => item.metadata.name !== 'flyer'
+  );
   const subarrayLength = Math.floor(content.length / columns);
   const remainder = content.length % columns;
   let start = 0;
@@ -11,8 +15,19 @@ export const splitArray = (
 
   for (let i = 0; i < columns; i++) {
     const end = start + subarrayLength + (i < remainder ? 1 : 0);
-    chunks.push(content.slice(start, end));
+    chunks.push(contentWithoutFlyer.slice(start, end));
     start = end;
   }
+
+  if (flyer) {
+    const lastElement = chunks[0].pop();
+    const lastChunk = chunks.pop();
+    if (lastElement && lastChunk) {
+      lastChunk.push(lastElement);
+      return [[flyer, ...chunks[0]], ...chunks.slice(1), lastChunk];
+    }
+    return [[flyer, ...chunks[0]], ...chunks.slice(1)];
+  }
+
   return chunks;
 };
