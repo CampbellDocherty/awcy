6461adb990740c539397ffad72dbc7a06ab733b2
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-05-04 16:45:51 +0100

    Health change component

---
+++ src/pages/Game/components/HealthChange.tsx
@@ -0,0 +1,39 @@
+import { styled, keyframes } from 'styled-components';
+
+const moveHealth = () => keyframes`
+0% {
+    opacity: 1;
+    top: 50%;
+    left: 46%;
+}
+
+80% {
+    opacity: 1;
+}
+
+100% {
+    top: 3%;
+    left: 46%;
+    opacity: 0;
+}`;
+
+const HealthChangeText = styled.p<{ $color: string }>`
+  font-family: Nokia;
+  font-size: 20px;
+  position: absolute;
+  opacity: 0;
+  color: ${(props) => props.$color};
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: black;
+  animation: ${moveHealth} 1s ease;
+`;
+
+export const HealthChange = ({ healthChange }: { healthChange: number }) => {
+  const color = healthChange > 0 ? '#57df00' : 'red';
+  const prefix = healthChange > 0 ? '+' : '';
+  return (
+    <HealthChangeText $color={color}>{`${
+      prefix + healthChange
+    }hp`}</HealthChangeText>
+  );
+};
